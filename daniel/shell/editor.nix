ec99d24c14d3df2e3bf67d82b4a9eea152452738
Daniel <daniel@daniellaing.com>
2023-10-30 16:12:56 +0000

    Tidied up editor

--- daniel/shell/editor.nix
+++ daniel/shell/editor.nix
@@ -1,14 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  home = {
-    packages = with pkgs; [
-      neovim
-    ];
+  programs.neovim = {
+    enable = true;
+  };
 
+  home = {
     sessionVariables = {
       EDITOR = "${pkgs.neovim}/bin/nvim";
       SUDO_EDITOR = "${pkgs.neovim}/bin/nvim";
+    };
+  };
+
+  xdg = {
+    desktopEntries = {
+      neovim = {
+        name = "Neovim";
+        genericName = "Text Editor";
+        comment = "Hyperextensible Vim-based text editor";
+        exec = "${pkgs.neovim}/bin/nvim %U";
+        terminal = true;
+        categories = [ "Utility" "TextEditor" "ConsoleOnly" ];
+        mimeType = [ "text/*" ];
+      };
+    };
+    mimeApps.defaultApplications = {
+      "text/*" = "neovim.desktop";
     };
   };
 }
