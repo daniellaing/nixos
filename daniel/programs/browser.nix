9cdc8eadc26113e02a95ade7b7b84a96daad2d18
Daniel <daniel@daniellaing.com>
2023-10-31 16:56:17 +0000

    Removed desktop entries for the web browsers

--- daniel/programs/browser.nix
+++ daniel/programs/browser.nix
@@ -10,30 +10,32 @@
     };
   };
 
-  xdg = {
-    desktopEntries = {
-      librewolf = {
-        name = "Librewolf";
-        genericName = "Web Browser";
-        comment = "Surf the web";
-        exec = "${pkgs.librewolf}/bin/librewolf %U";
-        categories = [ "Network" "WebBrowser" ];
-        mimeType = [ "x-scheme-handler/http" "x-scheme-handler/https" ];
-      };
-      firefox = {
-        name = "Firefox";
-        genericName = "Web Browser";
-        comment = "Surf the web";
-        exec = "${pkgs.firefox}/bin/firefox %U";
-        categories = [ "Network" "WebBrowser" ];
-        mimeType = [ "x-scheme-handler/http" "x-scheme-handler/https" ];
-      };
-    };
-    mimeApps.defaultApplications = {
-      "x-scheme-handler/http" = "librewolf.desktop";
-      "x-scheme-handler/https" = "librewolf.desktop";
-    };
-  };
+  # xdg = {
+  #   desktopEntries = {
+  #     librewolf = {
+  #       name = "Librewolf";
+  #       genericName = "Web Browser";
+  #       comment = "A fork of Firefox, focused on privacy, security and freedom";
+  #       icon = "Librewolf";
+  #       exec = "${pkgs.librewolf}/bin/librewolf %U";
+  #       categories = [ "Network" "WebBrowser" ];
+  #       mimeType = [ "x-scheme-handler/http" "x-scheme-handler/https" ];
+  #     };
+  #     firefox = {
+  #       name = "Firefox";
+  #       genericName = "Web Browser";
+  #       comment = "Mozilla Firefox, free web browser";
+  #       icon = "Firefox";
+  #       exec = "${pkgs.firefox}/bin/firefox %U";
+  #       categories = [ "Network" "WebBrowser" ];
+  #       mimeType = [ "x-scheme-handler/http" "x-scheme-handler/https" ];
+  #     };
+  #   };
+  #   mimeApps.defaultApplications = {
+  #     "x-scheme-handler/http" = "librewolf.desktop";
+  #     "x-scheme-handler/https" = "librewolf.desktop";
+  #   };
+  # };
 
   home.sessionVariables.BROWSER = "${pkgs.librewolf}/bin/librewolf";
 }
