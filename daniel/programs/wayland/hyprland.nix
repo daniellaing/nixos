c5c0fa6e91bbfcb2913b03097fa92ee9f8a47bdf
Daniel <daniel@daniellaing.com>
2023-11-10 15:28:19 +0000

    Set basic binds in hyprland

--- daniel/programs/wayland/hyprland.nix
+++ daniel/programs/wayland/hyprland.nix
@@ -5,15 +5,24 @@
     hyprland.homeManagerModules.default
   ];
 
+  home.packages = with pkgs; [
+    hyprpaper
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
-    # settings = {
-    #   input = {
-    #     kblayout = [ "gb" "us" ];
-    #     kbvariant = [ "uk" "dvorak" ];
-    #     kboptions = caps:swapescape,grp:ctrls_toggle;
-    #   };
-    # };
-    settings = { };
+    extraConfig = ''
+      input {
+              kb_layout = gb, us
+              kb_variant = uk, dvorak
+              kb_options = caps:swapescape,grp:ctrls_toggle
+              numlock_by_default = true
+              follow_mouse = 1
+          }
+
+      $MOD = SUPER
+      bind = $MOD, return, exec, kitty
+      bind = $MOD, backspace, exit,
+    '';
   };
 }
