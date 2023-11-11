e96b2efed5792ad83b6b43b80f1514a820cd2e17
Daniel <daniel@daniellaing.com>
2023-11-11 15:35:59 +0000

    Separated out font config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 let
@@ -18,6 +14,7 @@ in
   imports =
     [
       ./email
+      ./fonts.nix
       ./kde.nix
       ./mathematica.nix
       ./nix-locate.nix
@@ -125,11 +122,6 @@ in
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Fonts
-  fonts.fonts = with pkgs; [
-    nerdfonts
-  ];
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -151,47 +143,14 @@ in
   nixpkgs.config.allowAliases = false;
 
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
   services.dbus.packages = [
     pkgs.dbus.out
     config.system.path
   ];
   environment.pathsToLink = [ "/etc/dbus-1" "/share/dbus-1" ];
 
-  services.avahi.enable = true;
-  services.mysql = {
-    enable = true;
-    package = pkgs.mysql80;
-    ensureUsers = [
-      {
-        name = "daniel";
-        ensurePermissions = {
-          "*.*" = "ALL PRIVILEGES";
-        };
-      }
-    ];
-  };
-  # services.mongodb.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
   networking.firewall.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
   programs.ssh.askPassword = "/nix/store/pg42226jhbpjp47s03h0glzxyxq36h6i-ksshaskpass-5.27.7/bin/ksshaskpass";
