8ed1d54daf181561093611eb16b961528f3ec4d9
Daniel <daniel@daniellaing.com>
2023-11-17 09:42:01 +0000

    Bookmarks: Restructure to make toolbar work properly

--- daniel/programs/browsers/bookmarks.nix
+++ daniel/programs/browsers/bookmarks.nix
@@ -2,39 +2,48 @@
 
 [
   {
-    name = "Rices";
+    name = "Toolbar";
     toolbar = true;
     bookmarks = [
+
       {
-        name = "Summer day/night";
-        url = "https://github.com/MathisP75/summer-day-and-night";
-      }
-      {
-        name = "Gako358";
-        url = "https://github.com/Gako358/dotfiles";
-      }
-    ];
-  }
-  {
-    name = "Colour Schemes";
-    toolbar = true;
-    bookmarks = [
-      {
-        name = "Gruvbox Material (medium dark)";
+        name = "Rices";
         bookmarks = [
           {
-            name = "GruvboxMaterial 1";
-            url = "https://coolors.co/292828-32302f-504945-665c54-bdae93-ddc7a1-ebdbb2-fbf1c7";
+            name = "Summer day/night";
+            url = "https://github.com/MathisP75/summer-day-and-night";
           }
           {
-            name = "GruvboxMaterial 2";
-            url = "https://coolors.co/ea6962-e78a4e-d8a657-a9b665-89b482-7daea3-d3869b-bd6f3e";
-          }
-          {
-            name = "GruvboxMaterial Image";
-            url = "https://user-images.githubusercontent.com/58662350/213884019-cbcd5f00-5bef-4a37-9139-0570770330b6.png";
+            name = "Gako358";
+            url = "https://github.com/Gako358/dotfiles";
           }
         ];
+      }
+      {
+        name = "Colour Schemes";
+        bookmarks = [
+          {
+            name = "Gruvbox Material (medium dark)";
+            bookmarks = [
+              {
+                name = "GruvboxMaterial 1";
+                url = "https://coolors.co/292828-32302f-504945-665c54-bdae93-ddc7a1-ebdbb2-fbf1c7";
+              }
+              {
+                name = "GruvboxMaterial 2";
+                url = "https://coolors.co/ea6962-e78a4e-d8a657-a9b665-89b482-7daea3-d3869b-bd6f3e";
+              }
+              {
+                name = "GruvboxMaterial Image";
+                url = "https://user-images.githubusercontent.com/58662350/213884019-cbcd5f00-5bef-4a37-9139-0570770330b6.png";
+              }
+            ];
+          }
+        ];
+      }
+      {
+        name = "YouTube";
+        url = "https://www.youtube.com";
       }
     ];
   }
