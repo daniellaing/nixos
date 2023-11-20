119f27223e8c8484ba2134490119895757e80633
Daniel <daniel@daniellaing.com>
2023-11-20 22:25:56 +0000

    NixOS: Removed mathematica

--- daniel/programs/X11/sxhkd.nix
+++ daniel/programs/X11/sxhkd.nix
@@ -6,7 +6,6 @@
     keybindings = {
       # "super + c" = pkgs.writeShellScript "script" "$TERMINAL -c calcurse";
       "super + e" = "$TERMINAL -c neomutt";
-      "super + m" = "Mathematica";
     };
   };
 }
