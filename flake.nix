536b280e4227afd06a5bb58e00ffa7e378b8fc00
Daniel <daniel@daniellaing.com>
2023-11-20 22:25:38 +0000

    Flake: Added stable packages overlay

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   description = "Daniel's configuration flake";
 
   inputs = {
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     hyprland.url = "github:hyprwm/Hyprland";
     nix-colors.url = "github:misterio77/nix-colors";
     sops-nix.url = "github:Mic92/sops-nix";
@@ -16,21 +18,21 @@
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, hyprland, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      stable-pkgs = import nixpkgs-stable {
-        inherit system;
-        config.allowUnfree = true;
+      overlay-stable = final: prev: {
+        stable = import nixpkgs-stable {
+          inherit system;
+          config.allowUnfree = true;
+        };
       };
     in
     {
       nixosConfigurations = {
         "nixos" = nixpkgs.lib.nixosSystem rec {
           inherit system;
-          specialArgs = { inherit inputs; inherit stable-pkgs; };
+          specialArgs = { inherit inputs; };
+
           modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-stable ]; })
             ./hosts/dellG5.nix
             ./nixos/configuration.nix
 
