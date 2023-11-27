34149f7d3fa92940bfae2e3a17c01f7c8c34165d
Daniel <daniel@daniellaing.com>
2023-11-27 10:38:55 +0000

    DellG5: Enabled tlp power management

--- hosts/dellG5.nix
+++ hosts/dellG5.nix
@@ -62,8 +62,23 @@
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # Cooling managment
+  # Cooling and battery managment
   services.thermald.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+    };
+  };
 
   services.xserver.libinput.enable = true;
 
