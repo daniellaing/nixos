8ab6d34fda09552c3bbad6d29d5924a7f62374a3
Daniel <daniel@daniellaing.com>
2023-11-28 19:12:01 +0000

    Browser: Added extensions

--- daniel/programs/browsers/default.nix
+++ daniel/programs/browsers/default.nix
@@ -6,67 +6,73 @@ in
   # Set default browser
   home.sessionVariables.BROWSER = "${pkgs.firefox}/bin/firefox";
 
-  programs = builtins.listToAttrs (map
-    (b: {
-      name = b;
-      value = {
-        enable = true;
-        profiles.daniel = {
-          bookmarks = import ./bookmarks.nix (inputs);
-          settings = {
-            "browser.toolbars.bookmarks.visibility" = "always";
-            "browser.startup.couldRestoreSession.count" = 2;
-          };
-          search = {
-            force = true;
-            default = "SearxNG";
-            engines = {
-              "SearxNG" = {
-                urls = [{
-                  template = "https://freesearch.club/search";
-                  params = [
-                    { name = "q"; value = "{searchTerms}"; }
-                  ];
-                }];
-              };
-              "Nix Packages" = {
-                urls = [{
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    { name = "type"; value = "packages"; }
-                    { name = "channel"; value = "unstable"; }
-                    { name = "query"; value = "{searchTerms}"; }
-                  ];
-                }];
-                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@np" ];
-              };
-              "Nix Options" = {
-                urls = [{
-                  template = "https://search.nixos.org/options";
-                  params = [
-                    { name = "type"; value = "packages"; }
-                    { name = "channel"; value = "unstable"; }
-                    { name = "query"; value = "{searchTerms}"; }
-                  ];
-                }];
-                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@no" ];
-              };
-              "Home Manager Options" = {
-                urls = [{
-                  template = "https://mipmip.github.io/home-manager-option-search";
-                  params = [
-                    { name = "query"; value = "{searchTerms}"; }
-                  ];
-                }];
-                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@hm" ];
+  programs = builtins.listToAttrs
+    (map
+      (b: {
+        name = b;
+        value = {
+          enable = true;
+          profiles.daniel = {
+            bookmarks = import ./bookmarks.nix (inputs);
+            settings = {
+              "browser.toolbars.bookmarks.visibility" = "always";
+              "browser.startup.couldRestoreSession.count" = 2;
+            };
+            extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+              keepassxc-browser
+              ublock-origin
+              sponsorblock
+            ];
+            search = {
+              force = true;
+              default = "SearxNG";
+              engines = {
+                "SearxNG" = {
+                  urls = [{
+                    template = "https://freesearch.club/search";
+                    params = [
+                      { name = "q"; value = "{searchTerms}"; }
+                    ];
+                  }];
+                };
+                "Nix Packages" = {
+                  urls = [{
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      { name = "type"; value = "packages"; }
+                      { name = "channel"; value = "unstable"; }
+                      { name = "query"; value = "{searchTerms}"; }
+                    ];
+                  }];
+                  icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                  definedAliases = [ "@np" ];
+                };
+                "Nix Options" = {
+                  urls = [{
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      { name = "type"; value = "packages"; }
+                      { name = "channel"; value = "unstable"; }
+                      { name = "query"; value = "{searchTerms}"; }
+                    ];
+                  }];
+                  icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                  definedAliases = [ "@no" ];
+                };
+                "Home Manager Options" = {
+                  urls = [{
+                    template = "https://mipmip.github.io/home-manager-option-search";
+                    params = [
+                      { name = "query"; value = "{searchTerms}"; }
+                    ];
+                  }];
+                  icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                  definedAliases = [ "@hm" ];
+                };
               };
             };
           };
         };
-      };
-    })
-    browsers);
+      })
+      browsers);
 }
