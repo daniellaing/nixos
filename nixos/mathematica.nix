3a01b8f7271df462b9466ba83165914c1409673a
Daniel <daniel@daniellaing.com>
2023-11-28 19:13:20 +0000

    NixOS: Wrapped Mathematica to fix issues, still need to fix desktop entry

--- nixos/mathematica.nix
+++ nixos/mathematica.nix
@@ -1,10 +1,25 @@
 { config, pkgs, lib, ... }:
+let
+  m =
+    let
+      wrapped = pkgs.writeShellScriptBin "mathematica" ''
+        rm -f ''${MATHEMATICA_USERBASE:-$HOME/.Mathematica}/Autoload/PacletManager/Configuration/FrontEnd/init_${pkgs.stable.mathematica.version}.0.m
+        exec ${pkgs.stable.mathematica}/bin/mathematica "$@"
+      '';
+    in
+    pkgs.symlinkJoin {
+      name = "mathematica";
+      paths = [
+        wrapped
+        pkgs.stable.mathematica
+      ];
+    };
+in
 {
-  environment.systemPackages = with pkgs.stable; [
-    mathematica
-  ];
-
-  environment.variables = {
-    QT_XCB_GL_INTEGRATION = "none";
+  environment = {
+    systemPackages = [ m ];
+    variables = {
+      QT_XCB_GL_INTEGRATION = "none";
+    };
   };
 }
