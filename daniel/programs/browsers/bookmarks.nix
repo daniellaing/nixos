00ea8fe797849f30d9762a196f175142908f7879
Daniel <daniel@daniellaing.com>
2023-11-29 15:19:00 +0000

    Bookmarks: Added old bookmarks in

--- daniel/programs/browsers/bookmarks.nix
+++ daniel/programs/browsers/bookmarks.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 [
+  # Toolbar
   {
     name = "Toolbar";
     toolbar = true;
@@ -9,14 +10,8 @@
       {
         name = "Rices";
         bookmarks = [
-          {
-            name = "Summer day/night";
-            url = "https://github.com/MathisP75/summer-day-and-night";
-          }
-          {
-            name = "Gako358";
-            url = "https://github.com/Gako358/dotfiles";
-          }
+          { name = "Summer day/night"; url = "https://github.com/MathisP75/summer-day-and-night"; }
+          { name = "Gako358"; url = "https://github.com/Gako358/dotfiles"; }
         ];
       }
       {
@@ -25,18 +20,9 @@
           {
             name = "Gruvbox Material (medium dark)";
             bookmarks = [
-              {
-                name = "GruvboxMaterial 1";
-                url = "https://coolors.co/292828-32302f-504945-665c54-bdae93-ddc7a1-ebdbb2-fbf1c7";
-              }
-              {
-                name = "GruvboxMaterial 2";
-                url = "https://coolors.co/ea6962-e78a4e-d8a657-a9b665-89b482-7daea3-d3869b-bd6f3e";
-              }
-              {
-                name = "GruvboxMaterial Image";
-                url = "https://user-images.githubusercontent.com/58662350/213884019-cbcd5f00-5bef-4a37-9139-0570770330b6.png";
-              }
+              { name = "GruvboxMaterial 1"; url = "https://coolors.co/292828-32302f-504945-665c54-bdae93-ddc7a1-ebdbb2-fbf1c7"; }
+              { name = "GruvboxMaterial 2"; url = "https://coolors.co/ea6962-e78a4e-d8a657-a9b665-89b482-7daea3-d3869b-bd6f3e"; }
+              { name = "GruvboxMaterial Image"; url = "https://user-images.githubusercontent.com/58662350/213884019-cbcd5f00-5bef-4a37-9139-0570770330b6.png"; }
             ];
           }
         ];
@@ -44,41 +30,109 @@
       {
         name = "Jobs";
         bookmarks = [
-          {
-            name = "Babcock Data Analytics";
-            url =
-              "https://www.gradcracker.com/hub/113/babcock-international-group/graduate-job/54621/data-analytics-graduate-programme";
-          }
-          {
-            name = "Lloyds Data Science";
-            url =
-              "https://www.gradcracker.com/hub/275/lloyds-banking-group/graduate-job/56154/data-science-and-analytics-graduate-scheme";
-          }
-          {
-            name = "Jacobs Graduate Consultant";
-            url =
-              "https://www.gradcracker.com/hub/41/jacobs/graduate-job/56588/graduate-consultants-data-analysis";
-          }
-          {
-            name = "EY Actuarial Graduate";
-            url =
-              "https://www.gradcracker.com/hub/615/ey/graduate-job/55565/actuarial-graduate-programmes";
-          }
-          {
-            name = "NNL Graduate Scheme";
-            url =
-              "https://www.gradcracker.com/hub/168/national-nuclear-laboratory/graduate-job/54166/graduate-scheme-2024-science-and-engineering";
-          }
+          { name = "Babcock Data Analytics"; url = "https://www.gradcracker.com/hub/113/babcock-international-group/graduate-job/54621/data-analytics-graduate-programme"; }
+          { name = "Lloyds Data Science"; url = "https://www.gradcracker.com/hub/275/lloyds-banking-group/graduate-job/56154/data-science-and-analytics-graduate-scheme"; }
+          { name = "Jacobs Graduate Consultant"; url = "https://www.gradcracker.com/hub/41/jacobs/graduate-job/56588/graduate-consultants-data-analysis"; }
+          { name = "EY Actuarial Graduate"; url = "https://www.gradcracker.com/hub/615/ey/graduate-job/55565/actuarial-graduate-programmes"; }
+          { name = "NNL Graduate Scheme"; url = "https://www.gradcracker.com/hub/168/national-nuclear-laboratory/graduate-job/54166/graduate-scheme-2024-science-and-engineering"; }
+        ];
+      }
+
+      { name = "NixOS Wiki"; url = "https://nixos.wiki/wiki/Main_Page"; }
+      { name = "YouTube"; url = "https://www.youtube.com"; }
+      { name = "Encycolorpedia"; url = "https://encycolorpedia.com/"; }
+    ];
+  }
+
+  # Other Bookmarks
+  {
+    name = "LaTeX";
+    bookmarks = [
+      {
+        name = "TikZ";
+        bookmarks = [
+          { name = "Labeled Angle"; url = "https://tex.stackexchange.com/questions/20826/label-angle-with-tikz"; }
+        ];
+      }
+      { name = "LaTeX Cheat Sheet"; url = "https://wch.github.io/latexsheet/latexsheet-a4.pdf"; }
+      { name = "LaTeX Symbols"; url = "https://oeis.org/wiki/List_of_LaTeX_mathematical_symbols"; }
+      { name = "LaTeX Symbols (Big List)"; url = "https://rpi.edu/dept/arc/training/latex/LaTeX_symbols.pdf"; }
+      { name = "LaTeX Spacing Guide"; url = "https://tex.stackexchange.com/questions/74353/what-commands-are-there-for-horizontal-spacing"; }
+      { name = "\\@startsection"; url = "http://joshua.smcvt.edu/latex2e/bs-at-startsection.html"; }
+      { name = "Smart Multicol List"; url = "https://tex.stackexchange.com/a/194476"; }
+    ];
+  }
+
+  {
+    name = "Games";
+    bookmarks = [
+      {
+        name = "Factorio";
+        bookmarks = [
+          { name = "Autotorio"; url = "https://www.autotorio.com"; }
+          { name = "Re-enable Achievements"; url = "https://forums.factorio.com/viewtopic.php?f=5&t=29709"; }
+          { name = "Final (Maxed) Designs"; url = "https://forums.factorio.com/viewtopic.php?f=202&t=12588"; }
+          { name = "Factorio Cheat Sheet"; url = "https://factoriocheatsheet.com/"; }
+          { name = "GreyGooMk1"; url = "https://imgur.com/a/Ewhsv"; }
+          { name = "Speedrun Guide"; url = "https://docs.google.com/presentation/d/1XgyTdHzQM1cQrv1YpZJuRGtMv6AE9j4h6Phdn4Fe8-c/edit#slide=id.g91035a48d7_686_608"; }
         ];
       }
       {
-        name = "NixOS Wiki";
-        url = "https://nixos.wiki/wiki/Main_Page";
+        name = "Elite Dangerous";
+        bookmarks = [
+          { name = "EDDB"; url = "https://eddb.io"; }
+          { name = "Welcome Guide"; url = "https://www.reddit.com/r/EliteDangerous/comments/7na89c/welcome_to_elite_dangerous_new_commanders"; }
+        ];
       }
-      {
-        name = "YouTube";
-        url = "https://www.youtube.com";
-      }
+
+      { name = "EU4 Army Composition"; url = "https://docs.google.com/spreadsheets/d/1ITH6oNHsIlVHo2LJnR92wP5LEKiON0k2rZJ82YbYaB0/htmlview#gid=0"; }
     ];
   }
+
+  {
+    name = "Typing";
+    bookmarks = [
+      { name = "10 Fast Fingers"; url = "https://10fastfingers.com"; }
+      { name = "Speed Test"; url = "https://10fastfingers.com/typing-test/english"; }
+      { name = "keybr"; url = "https://www.keybr.com"; }
+      { name = "Typing Club"; url = "https://www.typingclub.com"; }
+      { name = "Typeracer"; url = "https://play.typeracer.com"; }
+      { name = "Monkey Type"; url = "https://monkeytype.com"; }
+      { name = "ztype"; url = "https://zty.pe"; }
+    ];
+  }
+
+  {
+    name = "Pens";
+    bookmarks = [
+      { name = "Muji Pen"; url = "https://www.muji.us/collections/pen-pencils"; }
+      { name = "Monteverde Invincia"; url = "https://www.gouletpens.com/collections/monteverde-invincia-fountain-pens/products/monteverde-invincia-color-fusion-fountain-pen-stealth-black"; }
+    ];
+  }
+  {
+    name = "Inks";
+    bookmarks = [
+      { name = "Noodler's Walnut"; url = "https://www.purepens.co.uk/products/noodler-s-walnut-fountain-pen-ink"; }
+      { name = "Noodler's Apache Sunset"; url = "https://www.gouletpens.com/products/noodlers-southwest-sunset-3oz-bottled-ink"; }
+      { name = "Diamine Sunshine Yellow"; url = "https://www.diamineinks.co.uk/detail.aspx?prodcode=040"; }
+      { name = "Diamine Graphite"; url = "https://www.gouletpens.com/products/diamine-graphite-30ml-bottled-ink"; }
+      { name = "Jacques Herbin Emerald of Chivor"; url = "https://www.gouletpens.com/products/jacques-herbin-1670-emerald-chivor-50ml-bottled-ink"; }
+    ];
+  }
+
+  {
+    name = "Stock Photos";
+    bookmarks = [
+      { name = "Pexels"; url = "https://www.pexels.com/"; }
+      { name = "Unsplash"; url = "https://unsplash.com/"; }
+      { name = "Coverr"; url = "https://coverr.co/"; }
+      { name = "Mazwai"; url = "https://mazwai.com/#/"; }
+    ];
+  }
+
+  { name = "NotInTheCurriculum"; url = "https://charm-infinity-fe8.notion.site/NITC-PF-169e60bc1230470690ff4b632a842fd8"; }
+  { name = "Github does dotfiles"; url = "https://dotfiles.github.io/"; }
+  { name = "POSIX Bible"; url = "https://github.com/dylanaraps/pure-sh-bible"; }
+  { name = "Tufte Pandoc CSS"; url = "https://jez.io/tufte-pandoc-css/"; }
+  { name = "Men's Knitted Scarf"; url = "https://interunet.com/how-to-knit-a-scarf-for-a-man"; }
 ]
