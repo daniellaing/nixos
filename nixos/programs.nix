5c13cd60f12461d73c5aa7d917725aa62cb72d66
Daniel <daniel@daniellaing.com>
2024-03-22 12:30:51 +0000

    NixOS: Added rebuild script

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -1,14 +1,39 @@
-{ pkgs, ... }:
-let
-  pypkgs = ps: with ps; [
-    numpy
-    requests
-    pandas
-    pip
-  ];
-in
-{
+{pkgs, ...}: let
+  pypkgs = ps:
+    with ps; [
+      numpy
+      requests
+      pandas
+      pip
+    ];
+
+  rebuild-script =
+    pkgs.writeShellApplication
+    {
+      name = "configure";
+      runtimeInputs = with pkgs; [git bat ripgrep libnotify];
+      text = ''
+        set -ex
+        $EDITOR /dotfiles
+        pushd /dotfiles
+        if git diff --quiet '*.nix'; then
+            echo "No changes detected, exiting"
+            popd
+            extit 0
+        fi
+        nix fmt &> /dev/null
+        git diff -U0 '*.nix'
+        # shellcheck disable=SC2024
+        sudo nixos-rebuild switch --flake /dotfiles &> nixos-rebuild.log || (notify-send -e -a "NixOS Rebuild" "Failure!"; bat nixos-rebuild.log | rg error && exit 1)
+        curgen=$(nixo-rebuild list-generations | rg current)
+        git commit -am "$curgen"
+        popd
+        notify-send -e -a "NixOS Rebuild" "Success!"
+      '';
+    };
+in {
   environment.systemPackages = with pkgs; [
+    rebuild-script
     # Utility
     git
     gcc
