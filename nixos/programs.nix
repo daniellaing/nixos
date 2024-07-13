b80890b187a7b370986016c13d192a81184e4998
Daniel <daniel@daniellaing.com>
2024-07-13 11:02:21 +0100

    Flake: Add `update-system` script to wrap updating nix flake
    590 current  2024-07-13 11:02:20  24.11.20240703.9f4128e  6.9.7-zen1                          *
    592 current  2024-07-13 11:18:23  24.11.20240709.feb2849  6.9.8-zen1                          *
    593 current  2024-07-13 11:41:26  24.11.20240709.feb2849  6.9.8-zen1                          *
    595 current  2024-07-13 11:55:04  24.11.20240709.feb2849  6.9.8-zen1                          *
    596 current  2024-07-13 12:00:27  24.11.20240709.feb2849  6.9.8-zen1                          *

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -37,9 +37,34 @@
         notify-send -e -a "NixOS Rebuild" "Success!"
       '';
     };
+
+  update-script =
+    pkgs.writeShellApplication
+    {
+      name = "update-system";
+      runtimeInputs = with pkgs; [git libnotify nh];
+      text = ''
+        set -ex
+        cfgdir="''${1:-/dotfiles}"
+        pushd "$cfgdir"
+        nix flake update --commit-lock-file
+
+        # nixos-rebuild switch --flake "$cfgdir"
+        # shellcheck disable=SC2024
+        nh os boot "$cfgdir" || (notify-send -e -a "NixOS Update", "Failure!"; exit 1)
+
+        msg=$(nixos-rebuild list-generations --flake "$cfgdir" | rg current)
+        old_msg=$(git log --format=%B -n1)
+        git commit --amend -m "$old_msg" -m "$msg"
+        popd
+        notify-send -e -a "NixOS Update" "Success!\n System will restart in 2 minutes"
+        shutdown -r 2
+      '';
+    };
 in {
   environment.systemPackages = with pkgs; [
     rebuild-script
+    update-script
     # Utility
     git
     gcc
