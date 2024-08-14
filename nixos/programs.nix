db72fa149417c28968c075f9d7829534688a7941
Daniel <daniel@daniellaing.com>
2024-08-14 21:27:33 +0100

    NixOS: Removed unnecessary packages 644 current  2024-08-14 21:27:32  24.11.20240809.5e0ca22  6.10.1-zen1                          *

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -1,46 +1,25 @@
-{pkgs, ...}: let
-  pypkgs = ps:
-    builtins.attrValues {
-      inherit
-        (ps)
-        numpy
-        requests
-        pandas
-        pip
-        ;
-    };
-in {
-  environment.systemPackages =
-    builtins.attrValues {
-      inherit
-        (pkgs)
-        # Utility
-        
-        git
-        gcc
-        unzip
-        wget
-        ripgrep
-        gnumake
-        pinentry-curses
-        gnupg
-        # (uutils-coreutils.override {prefix = "";}) # Coreutils rewrite in Rust btw
-        
-        # Secret management
-        
-        sops
-        # Desktop
-        
-        libreoffice
-        firefox
-        # Wayland
-        
-        wl-clipboard
-        ;
-    }
-    ++ [
-      pkgs.texlive.combined.scheme-full
-      (pkgs.uutils-coreutils.override {prefix = "";}) # Coreutils rewrite in Rust btw
-      (pkgs.python3.withPackages pypkgs)
-    ];
+{pkgs, ...}: {
+  environment.systemPackages = builtins.attrValues {
+    inherit
+      (pkgs)
+      # Utility
+      
+      git
+      unzip
+      wget
+      ripgrep
+      gnumake
+      pinentry-curses
+      gnupg
+      # Secret management
+      
+      sops
+      # Desktop
+      
+      libreoffice
+      # Wayland
+      
+      wl-clipboard
+      ;
+  };
 }
