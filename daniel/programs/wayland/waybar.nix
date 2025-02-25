09391f5c97d9a65e5e74cbbd6b01c2e16ca780d3
Daniel <daniel@daniellaing.com>
2025-02-25 20:46:16 +0000

    Waybar: Using default (non overriden) package. Can't remember the reason I was doing that anyway.
    704 current  2025-02-25 20:46:15  25.05.20250224.0196c01  6.13.3-zen1                          *

--- daniel/programs/wayland/waybar.nix
+++ daniel/programs/wayland/waybar.nix
@@ -9,9 +9,9 @@
 in {
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-    });
+    # package = pkgs.waybar.overrideAttrs (oldAttrs: {
+    #   mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    # });
     settings.mainBar = {
       "position" = "top";
       "layer" = "top";
