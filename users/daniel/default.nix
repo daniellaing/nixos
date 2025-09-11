0dc1ec3a1f271b007c504cc154a6a0b2a79d3966
Daniel Laing <daniel@daniellaing.com>
2025-09-11 08:49:56 +0100

    daniel/tmux: New panes and windows open in same directory as current Also enable passthrough

--- users/daniel/default.nix
+++ users/daniel/default.nix
@@ -7,14 +7,26 @@
       hyprland.enable = true;
       tmux = {
         conf = ''
+          # Vim keys for pane navigation
           bind h select-pane -L
           bind j select-pane -D
           bind k select-pane -U
           bind l select-pane -R
 
+          # Vim keys in copy mode
           bind-key -T copy-mode-vi v send-keys -X begin-selection
           bind-key -T copy-mode-vi V send-keys -X select-line
           bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+          # Open new panes and windows in same directory as current
+          bind '"' split-window -c "#{pane_current_path}"
+          bind % split-window -h -c "#{pane_current_path}"
+          bind c new-window -c "#{pane_current_path}"
+
+          # Enable passthrough
+          set -g allow-passthrough on
+          set -ga update-environment TERM
+          set -ga update-environment TERM_PROGRAM
         '';
       };
     };
