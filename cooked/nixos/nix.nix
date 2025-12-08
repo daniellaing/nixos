926c5e66b311ca510f9c0e5b3fb1c570a4ec9b86
Daniel Laing <daniel@daniellaing.com>
2025-12-08 11:31:37 +0000

    nix: Use GitHub access token if possible

--- cooked/nixos/nix.nix
+++ cooked/nixos/nix.nix
@@ -9,23 +9,35 @@ in {
     enable = lib.mkEnableOption "nix config";
   };
 
-  config = lib.mkIf cfg.enable {
-    nix = {
-      settings = {
-        experimental-features = ["nix-command" "flakes"];
-        auto-optimise-store = true;
-        use-xdg-base-directories = true;
-        trusted-users = ["root" "@wheel"];
+  config =
+    lib.recursiveUpdate
+    (lib.mkIf cfg.enable {
+      nix = {
+        settings = {
+          experimental-features = ["nix-command" "flakes"];
+          auto-optimise-store = true;
+          use-xdg-base-directories = true;
+          trusted-users = ["root" "@wheel"];
+        };
+        optimise = {
+          automatic = true;
+          dates = ["13:00" "20:00"];
+        };
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 14d";
+        };
       };
-      optimise = {
-        automatic = true;
-        dates = ["13:00" "20:00"];
+    })
+    # If we have sops, use a github access token
+    (lib.mkIf (cfg.enable && config.cooked.sops.enable) {
+      nix.extraOptions = ''
+        !include ${config.sops.secrets.nix-github-token.path}
+      '';
+
+      sops.secrets.nix-github-token = {
+        mode = "0440";
       };
-      gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 14d";
-      };
-    };
-  };
+    });
 }
