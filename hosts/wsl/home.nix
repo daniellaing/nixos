5a1eaac18cef236fc11aa2137023dd98defc91a5
Daniel Laing <daniel@daniellaing.com>
2026-05-14 13:55:54 +0100

    wsl: Update wsl config

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -1,11 +1,8 @@
 {pkgs, ...}: {
   home-manager.sharedModules = [
-    ({config, ...}: {
-      # Share ssh key with Windows
-      home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink /mnt/c/Users/daniel.laing/.ssh;
-
+    ({...}: {
       programs.git = {
-        package = pkgs.gitSVN;
+        package = pkgs.stable.gitSVN;
       };
     })
   ];
