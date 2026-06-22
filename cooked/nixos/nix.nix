826e015574031f61fa5951d63ea08055de9cea4d
Daniel Laing <daniel@daniellaing.com>
2026-06-22 10:00:38 +0100

    secrets: Remove nix github token

--- cooked/nixos/nix.nix
+++ cooked/nixos/nix.nix
@@ -9,35 +9,23 @@ in {
     enable = lib.mkEnableOption "nix config";
   };
 
-  config =
-    lib.recursiveUpdate
-    (lib.mkIf cfg.enable {
-      nix = {
-        settings = {
-          experimental-features = ["nix-command" "flakes"];
-          auto-optimise-store = true;
-          use-xdg-base-directories = true;
-          trusted-users = ["root" "@wheel"];
-        };
-        optimise = {
-          automatic = true;
-          dates = ["13:00" "20:00"];
-        };
-        gc = {
-          automatic = true;
-          dates = "weekly";
-          options = "--delete-older-than 14d";
-        };
+  config = lib.mkIf cfg.enable {
+    nix = {
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+        auto-optimise-store = true;
+        use-xdg-base-directories = true;
+        trusted-users = ["root" "@wheel"];
       };
-    })
-    # If we have sops, use a github access token
-    (lib.mkIf (cfg.enable && config.cooked.sops.enable) {
-      nix.extraOptions = ''
-        !include ${config.sops.secrets.nix-github-token.path}
-      '';
-
-      sops.secrets.nix-github-token = {
-        mode = "0440";
+      optimise = {
+        automatic = true;
+        dates = ["13:00" "20:00"];
       };
-    });
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 14d";
+      };
+    };
+  };
 }
