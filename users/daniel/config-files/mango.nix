cb8410f5440870601a71ead6123380501aa96553
Daniel Laing <daniel@daniellaing.com>
2026-08-28 21:45:02 +0100

    f

--- users/daniel/config-files/mango.nix
+++ users/daniel/config-files/mango.nix
@@ -13,4 +13,24 @@ in ''
   binds=${mod},W,spawn,$BROWSER
 
   bind=${mod},Return,spawn,${config.programs.terminal}
+
+  bind=${mod},1,view,1
+  bind=${mod},2,view,2
+  bind=${mod},3,view,3
+  bind=${mod},4,view,4
+  bind=${mod},5,view,5
+  bind=${mod},6,view,6
+  bind=${mod},7,view,7
+  bind=${mod},8,view,8
+  bind=${mod},9,view,9
+
+  bind=${mod}+SHIFT,1,tag,1
+  bind=${mod}+SHIFT,2,tag,2
+  bind=${mod}+SHIFT,3,tag,3
+  bind=${mod}+SHIFT,4,tag,4
+  bind=${mod}+SHIFT,5,tag,5
+  bind=${mod}+SHIFT,6,tag,6
+  bind=${mod}+SHIFT,7,tag,7
+  bind=${mod}+SHIFT,8,tag,8
+  bind=${mod}+SHIFT,9,tag,9
 ''
