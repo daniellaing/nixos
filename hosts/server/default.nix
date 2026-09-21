375bb6d8e70fc3f31e1c73f084be1c7f37e7eb60
Daniel Laing <daniel@daniellaing.com>
2026-09-21 16:58:59 +0100

    changes

---
+++ hosts/server/default.nix
@@ -0,0 +1,46 @@
+{
+  inputs,
+  lib,
+  config,
+  ...
+} @ args: let
+  users = ["sysadmin"];
+in {
+  imports = [
+    inputs.nixos-wsl.nixosModules.default
+  ];
+
+  home-manager.users = lib.mkHomeUsers ../../users args users;
+  # cooked.preload.server = true;
+  system.stateVersion = "23.05"; # Do not change.
+
+  users = {
+    users."sysadmin" = {
+      isNormalUser = true;
+      initialPassword = "password";
+      group = "sysadmin";
+    };
+
+    groups."sysadmin" = {};
+  };
+
+  virtualisation.vmVariant = {
+    virtualisation.forwardPorts = [
+      # forward local port 2222 -> 22, to ssh into the VM
+      {
+        from = "host";
+        host.port = 2222;
+        guest.port = 22;
+      }
+
+      # forward local port 80 -> 10.0.2.10:80 in the VLAN
+      {
+        from = "guest";
+        guest.address = "10.0.2.10";
+        guest.port = 80;
+        host.address = "127.0.0.1";
+        host.port = 80;
+      }
+    ];
+  };
+}
